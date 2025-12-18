2eda483802fdc246b7298f11f14769a507d11361
ralphVenancio <40677838+ralphVenancio@users.noreply.github.com>
2025-12-18 23:04:15 +0000

    Committing 1 item from workspace 0232b328-2857-4966-88dd-a7a6522d89db

---
+++ notebooks/NB_test.Notebook/notebook-content.py
@@ -0,0 +1,31 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+import pandas as pd
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
